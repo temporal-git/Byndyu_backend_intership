371a41b307b5d941232d6f5d08e4f39868d75f52
Vladislav <vladislav.tester@yandex.ru>
2024-07-19 03:18:50 +0300

    added decorator for time count in performance tests

--- performance/mass_sort_cython_counter.py
+++ performance/mass_sort_cython_counter.py
@@ -1,20 +1,22 @@
-from lowlevel import mass_sort_cython
+from lowlevel import mass_sort_cython as msc
 import time
 
-
-with open('../data/gen_arr_100m.txt', 'r') as file:
+with open('../data/gen_arr_1m.txt', 'r') as file:
     digi_list = [int(x) for x in file.read().splitlines()]
 
-start = time.time()
-print(mass_sort_cython.sum_of_two_min_elements_min(digi_list))
-end = time.time()
-cy_time = end - start
 
-start2 = time.time()
-print(mass_sort_cython.sum_of_two_min_elements_for(digi_list))
-end2 = time.time()
-cy_time2 = end2 - start2
+def time_of_function(function):
+    def wrapped(*args):
+        start_time = time.time()
+        result = function(*args)
+        end_time = time.time()
+        func_time = end_time - start_time
+        print(f'Time: {func_time}')
+        return result
+    return wrapped
 
 
-print(cy_time)
-print(cy_time2)
+decorated_cython_min = time_of_function(msc.sum_of_two_min_elements_min)
+decorated_cython_for = time_of_function(msc.sum_of_two_min_elements_for)
+print(f'Result: {decorated_cython_min(digi_list)}')
+print(f'Result: {decorated_cython_for(digi_list)}')
--- performance/mass_sort_test_perf.py
+++ performance/mass_sort_test_perf.py
@@ -1,9 +1,9 @@
 import time
-import mass_sort_FOR_cycle
-import mass_sort_MIN_function
-from lowlevel import mass_sort_cython
+import mass_sort_FOR_cycle as msfc
+import mass_sort_MIN_function as msmf
+from lowlevel import mass_sort_cython as msc
 
-start0 = time.time()
+load_start = time.time()
 # importing list of variables from .py file with 1000 units, can be easy added non digit symbols
 # from data.py_arr import digi_list
 
@@ -23,36 +23,28 @@ start0 = time.time()
 with open('../data/gen_arr_100m.txt', 'r') as file:
     digi_list = [int(x) for x in file.read().splitlines()]
 
-end0 = time.time()
-cy_time0 = end0 - start0
+load_end = time.time()
+load_time = load_end - load_start
+print(f'Load file time: {load_time} \n')
 
 
-start = time.time()
-print(f'Result Python FOR: {mass_sort_FOR_cycle.sum_of_two_min_elements_for(digi_list)}')
-end = time.time()
-cy_time = end - start
-
-start2 = time.time()
-print(f'Result Python MIN: {mass_sort_MIN_function.sum_of_two_min_elements_min(digi_list)}')
-end2 = time.time()
-cy_time2 = end2 - start2
-
-start3 = time.time()
-print(f'Result Cython FOR: {mass_sort_cython.sum_of_two_min_elements_for(digi_list)}')
-end3 = time.time()
-cy_time3 = end3 - start3
-
-start4 = time.time()
-print(f'Result Cython MIN: {mass_sort_cython.sum_of_two_min_elements_min(digi_list)} \n')
+def time_of_function(function):
+    def wrapped(*args):
+        start_time = time.time()
+        result = function(*args)
+        end_time = time.time()
+        func_time = end_time - start_time
+        # print(f'Time: {func_time}')
+        return result, f'Time: {func_time}'
+    return wrapped
 
 
-end4 = time.time()
-cy_time4 = end4 - start4
+decorated_python_for = time_of_function(msfc.sum_of_two_min_elements_for)
+decorated_python_min = time_of_function(msmf.sum_of_two_min_elements_min)
+decorated_cython_for = time_of_function(msc.sum_of_two_min_elements_for)
+decorated_cython_min = time_of_function(msc.sum_of_two_min_elements_min)
 
-print(f'Load  file time: {cy_time0}')
-print(f'Python FOR time: {cy_time}')
-print(f'Python MIN time: {cy_time2}')
-print(f'Cython FOR time: {cy_time3}')
-print(f'Cython MIN time: {cy_time4}')
-
-
+print(f'Result Python FOR: {decorated_python_for(digi_list)}')
+print(f'Result Python MIN: {decorated_python_min(digi_list)}')
+print(f'Result Cython FOR: {decorated_cython_for(digi_list)}')
+print(f'Result Cython MIN: {decorated_cython_min(digi_list)}')
